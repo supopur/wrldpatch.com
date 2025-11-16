8ab9e959bdaa340ba5a3f96fb6dec257082119aa
Projectmat1 <matylda.smyk.08@gmail.com>
2025-11-16 23:57:33 +0100

    First commit, init?

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<script setup>
+</script>
+
+<template>
+<Index></Index>
+</template>
+
+<script>
+import Index from "@/components/Index.vue";
+
+export default {
+  components: {
+    Index
+  }
+}
+
+
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/components/Index.vue
@@ -0,0 +1,24 @@
+
+
+<template>
+  Helloooo
+<h1>Response: {{getResponse()}}</h1>
+</template>
+
+<script>
+export default {
+  name: "Index",
+  methods: {
+    getResponse() {
+      return "Helloooo!";
+    }
+  }
+}
+
+
+
+</script>
+
+<style scoped>
+
+</style>
