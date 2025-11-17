5c025aff6a58168b97d7aa66c4470f4b90a3215f
Projectmat1 <matylda.smyk.08@gmail.com>
2025-11-17 01:11:04 +0100

    Yay it works

--- src/components/Index.vue
+++ src/components/Index.vue
@@ -2,17 +2,30 @@
 
 <template>
   Helloooo
-<h1>Response: {{getResponse()}}</h1>
+<h1>Response: {{ response }}</h1>
 </template>
 
 <script>
+import axios from 'axios'
+
 export default {
   name: "Index",
+  data() {
+    return {
+      response: null
+    };
+  },
   methods: {
     getResponse() {
-      return "Helloooo!";
+      axios.get('https://mpe31fbb95bcd70113f8.free.beeceptor.com/').then((response) => {
+        this.response = response.data;
+      })
     }
+  },
+  mounted() {
+    this.getResponse();
   }
+
 }
 
 
